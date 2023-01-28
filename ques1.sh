744a392b80df7ac75507428b1d6403ffe9af3e09
princeakhoury13 <122992091+princeakhoury13@users.noreply.github.com>
2023-01-28 10:25:13 +0530

    Update ques1.sh

--- ques1.sh
+++ ques1.sh
@@ -1,3 +1,19 @@
-#!/bin/bash -x
+#!/bin/bash
 
+
+numbers=()
+for i in {1..10}
+do
+  num=$((RANDOM%1000))
+  numbers+=($num)
+done
+
+echo "Original array: ${numbers[@]}"
+sorted_numbers=($(echo "${numbers[@]}" | tr ' ' '\n' | sort -n))
+
+echo "Sorted array: ${sorted_numbers[@]}"
+second_largest=${sorted_numbers[8]}
+echo "2nd Largest number: $second_largest"
+second_smallest=${sorted_numbers[1]}
+echo "2nd Smallest number: $second_smallest"
 
